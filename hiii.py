012cfc240d321dc09f5145c401b5fd77fb873252
BikalAdhikari10 <beekaladhikari10@gmail.com>
2025-06-03 17:23:27 +0545

    main

--- hiii.py
+++ hiii.py
@@ -2,9 +2,7 @@ def sum(a,b):
     return a+b
 print(sum(1,2))
 d={"bikal":2,"hi":3}
-<<<<<<< HEAD
-b=[(key,value) for key,value in d.items()]
-=======
+
 b=[key for key,value in d.items()]
->>>>>>> main
+
 print(b)
